c8e43014c7d0bccda8531647385182117d634e84
Thomas Hines <thomashines@gmail.com>
2016-12-07 20:31:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(input)
+    array = []
+    input.map do |x|
+      array << x if @word.chars.sort == x.chars.sort 
+    end
+    array
+  end
+
+end
+
+# learn --fail-fast || rspec --fail-fast
