325ffc581f03e96a13d21be008c509abdb0e0fc6
Jordan Sleeper <ezirens@gmail.com>
2016-12-14 06:08:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(words)
+    words.select do |word|
+      #binding.pry
+      #split word into each character then sort it then compare ==
+      #have to split and sort both
+      word.split('').sort == anagram.split('').sort
+    end
+  end
+
+end
