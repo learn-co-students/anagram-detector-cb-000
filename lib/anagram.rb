1bc5f55185a84e1c8763b65a2a5272a1aec016c1
Alexander Thibodeaux <alyxanderthibodeaux@gmail.com>
2016-12-16 16:19:23 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+  def match(array)
+    result = []
+    word = @word.chars
+    array.each do |item|
+      switch = true
+      i = 0
+      while i < word.length
+        switch = false if !item.include?(word[i]) || item.length != @word.length
+        i += 1
+      end
+      result << item if switch
+    end
+    result
+  end
+end
