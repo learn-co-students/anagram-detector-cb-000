35986129509110f21d4a6443d3549e3fad98ef62
Jesus Vega <chuyspc@gmail.com>
2017-01-04 00:07:46 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,10 +8,11 @@ class Anagram
   end
 
   def match(word_list)
-    original_list = word_list
-    output = []
-    word_list = word_list.map{|e| e.split("").sort}
-    word_list.each_with_index {|x,index| output << original_list[index] if x == @given_word }
-    output
+    # original_list = word_list
+    # output = []
+    # word_list = word_list.map{|e| e.split("").sort}
+    # word_list.each_with_index {|x,index| output << original_list[index] if x == @given_word }
+    # output
+    word_list.select {|word| @given_word.all? {|letter| word.split('').include?(letter)} && word.length == @given_word.length}
   end
 end
