718b00ce48464121808392b15aeb5cc08c9799af
Grant MacManus <flatiron@macdav.net>
2017-01-23 23:43:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,15 +10,3 @@ class Anagram
   end
 
 end
-
-
-#
-# attr_accessor :name
-#
-# def initialize(word)
-#   @name = word
-# end
-#
-# def match(array)
-#   array.select {|x| x.split("").sort == @name.split("").sort}
-# end
