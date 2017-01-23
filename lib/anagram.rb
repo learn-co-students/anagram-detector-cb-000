679e0d0e4407a17f626dab9100cf270a9519c073
Grant MacManus <flatiron@macdav.net>
2017-01-23 23:41:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word
+  end
+
+  def match(array)
+    array.select { |i| i.split("").sort == @name.split("").sort }
+  end
+
+end
+
+
+#
+# attr_accessor :name
+#
+# def initialize(word)
+#   @name = word
+# end
+#
+# def match(array)
+#   array.select {|x| x.split("").sort == @name.split("").sort}
+# end
