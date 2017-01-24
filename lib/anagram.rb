8bc2db24d9e35e502a07fa8450502b4c9c998b73
dustinlikens <dustinlikens@icloud.com>
2017-01-24 16:37:13 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matches = array.select do |candidate_word|
+      @word.split('').sort == candidate_word.split('').sort
+    end
+  end
+
+end
