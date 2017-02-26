4127b714123d5f87f04e94ccb26b564d780e2b2d
ibrahim abdullah <ibrahim.a.abdullah@gmail.com>
2017-02-26 05:55:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+# class Anagram to word on initialize
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(some_word)
+    some_word.select do |some|
+      some.split("").sort == @word.split("").sort
+    end
+  end
+
+end
