a28e1707103a0c6967a819c0df08e7c041b4ca26
Lizzie Sorin <lizzie.sorin@hotmail.fr>
2017-03-27 22:31:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    array_word = word.split('')
+    list.find_all do |w| w.split('').sort ==array_word.sort
+    end
+
+
+  end
+
+end
