24fb4f22d804e1b9b5f00daa4bc9bdc8291ee23f
Dan M <arconk@gmail.com>
2017-04-14 12:11:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word=word
+
+  end
+
+  def match(possibilities)
+    results = []
+    alphaword = @word.chars.sort.join
+    alphabetical = possibilities.collect{|word|  word.chars.sort.join}
+    alphabetical.each_with_index do |word,index|
+      if word == alphaword
+        results << possibilities[index]
+      end
+    end
+    results
+  end
+end
