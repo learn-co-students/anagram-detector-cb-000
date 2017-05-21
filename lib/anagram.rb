86af00130a8ef2614bef28ec01772ce0875db9dd
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-05-21 12:46:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize word
+    @word = word
+  end
+
+  def match words_match
+    word_letters_sorted = @word.split("").sort
+    words_match.select { |word_match| word_match.split("").sort == word_letters_sorted }
+  end
+
+end
