a1fef411077e656124d1fe427d49f5e68ea328b2
cptbrkfst <dyl@resonategroup.ca>
2017-06-08 16:52:04 -0400

    Completed lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(array)
+    matches = []
+    anagram_split = @anagram.split(//).sort_by(&:downcase)
+
+    array.each do |word|
+      word_split = word.split(//).sort_by(&:downcase)
+      matches << word if word_split == anagram_split
+    end
+    matches
+  end
+end
