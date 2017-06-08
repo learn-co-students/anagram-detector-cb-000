2c17fc9ba820f52d80b0bf3c36ac12f541268816
cptbrkfst <dyl@resonategroup.ca>
2017-06-08 17:04:00 -0400

    optimized selector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,13 +8,6 @@ class Anagram
   end
 
   def match(array)
-    matches = []
-    anagram_split = @anagram.split(//).sort_by(&:downcase)
-
-    array.each do |word|
-      word_split = word.split(//).sort_by(&:downcase)
-      matches << word if word_split == anagram_split
-    end
-    matches
+    array.select { |e| e.split(//).sort == @anagram.split(//).sort }
   end
 end
