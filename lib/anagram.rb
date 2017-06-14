cc2df9229c553f408e05ff0e3655e9b2a7ecd44b
Tony Nassif <tonynassif@gmail.com>
2017-06-14 13:02:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,15 @@
 class Anagram
+  attr_accessor :word
 
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_list)
+    word_splitter = @word.split("").sort
+    word_list.any? do |x|
+      list_word_split = x.split("").sort
+      list_word_split == word_splitter
+    end
+  end
 end
