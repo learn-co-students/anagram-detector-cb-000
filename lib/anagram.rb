aeeba00ebe91d11f39a80b270f779d00c0573220
Marcus Havanek <marcus.havranek@havranek.co.at>
2017-06-17 10:02:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(words)
+    words.find_all{ |word| word.split(//).sort == @anagram.split(//).sort }
+  end
+
+end
+
