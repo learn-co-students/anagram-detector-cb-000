3a777ceb3d26a156253dfeec717aa3e61e3d7a9c
Patrick Townley <patricktownley@gmail.com>
2017-07-31 00:51:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_list)
+    to_match = @word.downcase.chars.sort.join
+    matches = []
+    word_list.each do |possible_match|
+      if possible_match.downcase.chars.sort.join == to_match
+        matches << possible_match
+      end
+    end
+    matches
+  end
+end
