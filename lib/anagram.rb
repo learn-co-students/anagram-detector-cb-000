84ca2054da5d841619a3e28b1552379049b98ad1
Lashaunna Samuels <shaunna191@hotmail.com>
2017-08-18 19:14:46 -0400

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require 'Pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(ana)
+    # binding.pry
+    ana.delete_if do |ana|
+      ana.split('').sort != @word.split('').sort
+    end
+  end
+end
