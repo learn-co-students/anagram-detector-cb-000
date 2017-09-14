e99350b8c4a39e3b0871504d4cdaa70ebedf0ee1
Muhammad Zeeshan Shafqat <101041699@student.swin.edu.au>
2017-09-14 21:00:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
 # Your code goes here!
+require "pry"
+class Anagram
+  attr_accessor :word
+
+ def initialize(possible_anagrams)
+   @word = possible_anagrams
+ end
+
+ def match(array_of_anagrams)
+   n = []
+   b = []
+   #binding.pry
+   array_of_anagrams.each do |a|
+    #binding.pry
+     a = a.split("")
+     @word = @word.split("")
+     if a.sort == @word.sort
+       a = a.join("")
+       @word = @word.join("")
+       n << a
+     else
+       @word = @word.join("")
+    end
+  end
+  if n.empty?
+      b
+  else
+    n
+  end
+ end
+end
