fd40d66cd3b850f5e2d5b9d936c1230ed8ee5213
JR <jrfrazier21@gmail.com>
2018-02-03 14:25:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :anagram
+  def initialize(x)
+      @anagram = x.split('').sort.join
+      @anagram
+  end
+  def match(x)
+    x.find_all {|i| i.split('').sort.join == @anagram}
+  end
+
+end
+binding.pry
