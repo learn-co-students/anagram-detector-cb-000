d2179f173f4c41270c3d05afe92c0c2c84609a03
Allan Glasier <allan.glasier@outlook.com>
2018-04-08 03:48:44 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,11 +7,8 @@ class Anagram
   end
 
   def match(array)
-    # --Original--
-    # match = []
-    # array.select {|x| match << x if x.split("").sort.join("") == @word.split("").sort.join("")}
-
-    array.select {|x| x.split("").sort == @name.split("").sort}
+    match = []
+    array.select {|x| match << x if x.split("").sort.join("") == @word.split("").sort.join("")}
   end
 
 end
