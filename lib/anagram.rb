23ecaa04ee0deb249902e04ebd0eaec7e7996346
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-23 20:32:08 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_reader :word
+  def initialize(word)
+    @word = word
+  end
+  def match(anagrams)
+    matches = []
+    anagrams.each do |anagram|
+      if anagram.size == @word.size
+        if @word.split('').all? {|letter_in_word| @word.count(letter_in_word) == anagram.count(letter_in_word)}
+          matches << anagram
+        end
+      end
+    end
+    matches
+  end
+end
