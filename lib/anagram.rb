5f5b79ffd7833f66a3a34ccccb099ea98db6dab7
abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-05-02 04:06:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor
+  @@all = []
+
+  def initialize(word)
+    @word = word
+    @@all << self
+  end
+
+  def match(word_array)
+    anagrams = []
+    word_array.each{|word| if @word.chars.sort.join == word.chars.sort.join
+      anagrams << word
+      end
+      }
+      anagrams
+  end
+
+end
