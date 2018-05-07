dd20793f3d65d9b34cd586b9d2278cbd0cbda0ad
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-07 20:39:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram_candidates)
+    matcher = word.split("")
+    matches = []
+    anagram_candidates.collect do |option|
+      option_array = option.split("")
+        if option_array.sort == matcher.sort
+          matches << option
+        end
+      end
+      matches
+  end
+
+end
