2bec30a12bf1ca99f48e7e5184f9bd652823eeed
Adam Urbina <adamurbina@gmail.com>
2018-06-08 19:50:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+    def initialize(input_word)
+        @input = input_word.split("").sort.join
+    end
+
+    def match(possible_array)
+        anagrams = []
+        possible_array.each { |word|
+            new_word = word.split("").sort.join
+            if new_word == @input
+                anagrams << word
+            end
+        }
+        anagrams
+    end
+
+end
