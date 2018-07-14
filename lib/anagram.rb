0334146a052d206360f14070e5d743ed81af05c9
jmoypace <jmoypace@gmail.com>
2018-07-14 21:55:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :key
+
+    def initialize(word)
+      @key = word
+    end
+
+    def match(array)
+      array.select {|x| x.split("").sort == @key.split("").sort}
+    end
+
+end
