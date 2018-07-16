50faeace86e791e5ad6648bf162d7eeb2ec7b5ba
Tao <werewaswas@live.com>
2018-07-16 07:25:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram 
+  def initialize(word) 
+    @word = word
+    @wordsplitSort = splitSort(word)
+  end
+  
+  def splitSort(word)
+    word.split('').sort
+  end
+  
+  def match(words)
+    words.select do 
+      # |word| @word.split('').sort == word.split('').sort  
+      # |word| splitSort(@word) == splitSort(word) 
+      |word| @wordsplitSort == splitSort(word)
+    end
+  end
+end
