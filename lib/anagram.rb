84e3acaadf9a7bb51d0fa3e7a026965208f9203b
josh.e.stroud@gmail.com Stroud <josh.e.stroud@gmail.com>
2018-08-08 05:35:40 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :match_word 
+  
+  def initialize(match_word)
+    @match_word = match_word 
+  end
+  
+  def match(possible_words)
+    matched_words = []
+    possible_words.each do |word|
+      if word.split("").sort == @match_word.split("").sort
+        matched_words.push(word)
+      end
+    end
+    matched_words
+  end
+end
