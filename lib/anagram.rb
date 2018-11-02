0628f732cdcc9ce5902ce9b0e9eb76566b4ba30a
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-11-02 05:13:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match(array_words)
+    return_array = []
+      array_words.each do |new_word|
+        if @word.split("").sort == new_word.split("").sort
+          return_array << new_word
+        end
+      end
+      return_array
+    end
+end
