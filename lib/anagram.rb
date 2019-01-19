298180b2709d33600315870fe4e28cbc0629c012
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-19 13:18:32 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+    @word_array = word.split("")
+  end
+
+  def match(array)
+    array.select{|word| word if word.split("").sort == @word_array.sort}
+  end
+end
