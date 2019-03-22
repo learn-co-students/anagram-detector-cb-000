da4129db556050000c49d20da41e20be85453d57
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-22 02:04:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @anagrams_array = []
+    @word = String.new
+    @word = word
+  end
+  def match(input_array)
+    input_array.each do |possible_anagram|
+      if possible_anagram.split("").sort == @word.split("").sort && possible_anagram != @word
+        @anagrams_array << possible_anagram
+      end
+    end
+    @anagrams_array
+  end
+end
+
+hello = Anagram.new("hello")
+puts hello.match(["trekking", "hiking", "lehol", "stop", "stupid", "olleh"])
