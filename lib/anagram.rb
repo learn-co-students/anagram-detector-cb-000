a971326912c2c5dbf9b288b6853ec7e5d7077922
Will Hodgson <will.hodgson@current-rms.com>
2019-08-05 11:02:52 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word_list, :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_list)
+    anagrams = []
+    word_list.each do |word|
+      split_word = word.split('').sort
+      split_instance_word = @word.split('').sort
+      
+      if split_word == split_instance_word
+        anagrams << word
+      end
+    end
+    anagrams
+  end
+
+end
